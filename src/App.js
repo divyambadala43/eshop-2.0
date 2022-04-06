6eadcc19aeaae57b87dcbc21577529d5748aeb69
Divya <divyambadala43@gmail.com>
2022-04-06 14:35:45 +0530

    ADD: Setup routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Product from "./components/Product";
+import Contact from "./components/Contact";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route to="/" element={<Home />} />
+        <Route to="/products" element={<Product />} />
+        <Route to="/about" element={<About />} />
+        <Route to="/contact" element={<Contact />} />
+        {/* <Redirect /> */}
+      </Routes>
+    </>
+  );
 }
 
 export default App;
