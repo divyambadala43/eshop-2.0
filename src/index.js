ca33e2692e3004cf2b891a67c14ccc039f2151e7
Divya <divyambadala43@gmail.com>
2022-04-07 15:45:36 +0530

    ADD: created redux store and integrated it to the app

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import addItem from "./additem";
+
+const rootReducers = combineReducers({
+  addItem,
+});
+
+export default rootReducers;
